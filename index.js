ba97c102ce7808fd179fc33536d474f2acbec770
Domi <domi.creativespace.gmail.com>
2023-03-22 14:26:52 +0700

    Hover select on gender radio

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
+//#region variables and DOM queries
+let selectedButton = null;
+const genderButtons = document.querySelectorAll(".gender-radio");
 const ageSlider = document.getElementById('age-slider');
 const ageInput = document.getElementById('age-input');
+//#endregion of variable and DOM queries
 
-// default value
+//#region default value
 ageSlider.value = 18;
 ageInput.value = 18;
 
-console.log("slider", ageSlider);
-console.log("input", ageInput);
+//#endregion of default value
 
 ageSlider.addEventListener("input", () => {
   ageInput.value = ageSlider.value;
@@ -15,3 +18,35 @@ ageSlider.addEventListener("input", () => {
 ageInput.addEventListener("input", () => {
   ageSlider.value = ageInput.value;
 })
+
+genderButtons.forEach(function(button) {
+  button.addEventListener("mouseenter", function() {
+    genderButtons.forEach(function(btn) {
+      btn.classList.remove("hoverselect");
+      const img = btn.querySelector("img");
+      img.src = img.src.replace("-hover.png", "-gray.png");
+    });
+
+    this.classList.add("hoverselect")
+    const img = this.querySelector("img");
+    img.src = img.src.replace("-gray.png", "-hover.png");
+  });
+
+  button.addEventListener("mouseleave", function() {
+    if(this !== selectedButton){
+      this.classList.remove("hoverselect")
+      const img = this.querySelector("img");
+      img.src = img.src.replace("-hover.png", "-gray.png");
+    }
+
+    if(selectedButton){
+      selectedButton.classList.add("hoverselect");
+      const selected_img = selectedButton.querySelector("img");
+      selected_img.src = selected_img.src.replace("-gray.png", "-hover.png");
+    }
+  });
+
+  button.addEventListener("click", function() {
+    selectedButton = button;
+  });
+});
